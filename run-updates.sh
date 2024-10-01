40c5ecb4cd0aa09c036a2b0f116d11313c2c8ded
Angel Guevara, Jr. <angel.guevara@trackforce.com>
2024-10-01 14:33:45 -0400

    Added exit code output

--- run-updates.sh
+++ run-updates.sh
@@ -12,8 +12,10 @@ echo '**************************'
 apt update -y
 updateExitCode=$?
 if [ $updateExitCode -ne 0 ]; then
-    echo -e '\033[31mError updating package lists, exit code $updateExitCode\033[0m'
+    echo -e '\033[31mError updating package lists, exit code' $updateExitCode '\033[0m'
     exit $updateExitCode
+else
+    echo -e '\033[32mPackage lists updated with exit code' $updateExitCode '\033[0m'
 fi
 
 echo ''
@@ -24,8 +26,10 @@ echo '*******************************'
 apt list --upgradable -a
 listExitCode=$?
 if [ $listExitCode -ne 0 ]; then
-    echo -e '\033[31mError listing upgradable packages, exit code $listExitCode\033[0m'
+    echo -e '\033[31mError listing upgradable packages, exit code' $listExitCode '\033[0m'
     exit $listExitCode
+else
+    echo -e '\033[32mUpgradable packages listed with exit code' $listExitCode '\033[0m'
 fi
 
 echo ''
@@ -36,8 +40,10 @@ echo '**********************'
 apt upgrade -y
 upgradeExitCode=$?
 if [ $upgradeExitCode -ne 0 ]; then
-    echo -e '\033[31mError upgrading packages, exit code $upgradeExitCode\033[0m'
+    echo -e '\033[31mError upgrading packages, exit code' $upgradeExitCode '\033[0m'
     exit $upgradeExitCode
+else
+    echo -e '\033[32mPackages upgraded with exit code' $upgradeExitCode '\033[0m'
 fi
 
 echo ''
@@ -48,8 +54,10 @@ echo '*************************'
 apt dist-upgrade -y
 distUpgradeExitCode=$?
 if [ $distUpgradeExitCode -ne 0 ]; then
-    echo -e '\033[31mError installing OS updates, exit code $distUpgradeExitCode\033[0m'
+    echo -e '\033[31mError installing OS updates, exit code' $distUpgradeExitCode '\033[0m'
     exit $distUpgradeExitCode
+else
+    echo -e '\033[32mOS updates installed with exit code' $distUpgradeExitCode '\033[0m'
 fi
 
 echo ''
@@ -60,8 +68,10 @@ echo '**************************'
 apt clean
 cleanExitCode=$?
 if [ $cleanExitCode -ne 0 ]; then
-    echo -e '\033[31mError cleaning package cache, exit code $cleanExitCode\033[0m'
+    echo -e '\033[31mError cleaning package cache, exit code' $cleanExitCode '\033[0m'
     exit $cleanExitCode
+else
+    echo -e '\033[32mPackage cache cleaned with exit code' $cleanExitCode '\033[0m'
 fi
 
 echo ''
@@ -72,8 +82,10 @@ echo '****************************'
 apt autoremove -y
 autoremoveExitCode=$?
 if [ $autoremoveExitCode -ne 0 ]; then
-    echo -e '\033[31mError removing unused packages, exit code $autoremoveExitCode\033[0m'
+    echo -e '\033[31mError removing unused packages, exit code' $autoremoveExitCode '\033[0m'
     exit $autoremoveExitCode
+else
+    echo -e '\033[32mUnused packages removed with exit code' $autoremoveExitCode '\033[0m'
 fi
 
 echo ''
